528e8c6941d2342bf70398d6ed8925a4466db86e
shaumikkalwit <shaumik.kalwit@gmail.com>
2020-05-13 19:45:39 -0700

    init after restart

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg" position="sticky" fixed="top">
+        <Navbar.Brand href="#home">Shaumik Kalwit</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">About Me</Nav.Link>
+          <Nav.Link href="#home">Projects</Nav.Link>
+          <Nav.Link href="#home">Contacts</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
